654c61a6384490c3ccfd3519b81915be9e7a12d8
Ahmet CANKURT <myahmetcankurt@gmail.com>
2025-03-20 18:35:54 +0300

    g

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -14,6 +14,7 @@ import Sidebar from "../../Component/Sidebar";
 import { useDispatch } from "react-redux";
 import { getUsers } from "../../redux/slices/userInformation";
 import useMobileMode from "../../hooks/useMobileMode";
+
 import "./Home.css";
 
 const App = () => {

e71835aa2bcdf6b806abe19705469c2018bf5638
Ahmet CANKURT <myahmetcankurt@gmail.com>
2025-03-24 16:53:07 +0300

    gg

--- src/Component/UserImage.js
+++ src/Component/UserImage.js
@@ -1,21 +1,32 @@
-import { memo } from "react";
+import { memo, useEffect, useState } from "react";
 import { API_URL } from "../config";
 import ProfileBg from "../assets/image/not-user-image-3.png";
 import "./UserImage.css";
 
 const UserImage = ({ src, alt }) => {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    if (loaded) {
+      setTimeout(() => {
+        setLoaded(true);
+      }, 2000);
+    }
+  }, [loaded]);
 
   return (
-    <div className=" me-2">
-        <img
-          src={`${API_URL}/${src}`}
-          className="chat-img-me loaded"
-          alt={alt || "User image"}
-          onError={(e) => {e.target.src = ProfileBg}}
-          />
-      {/* {isActive && <span className="status-light active" />} */}
+    <div className="me-2">
+      <img
+        src={`${API_URL}/${src}`}
+        className={`chat-img-me ${loaded ? "loaded" : "loading"}`}
+        alt={alt || "User image"}
+        onLoad={() => setLoaded(true)}
+        onError={(e) => {
+          e.target.src = ProfileBg;
+        }}
+      />
     </div>
   );
-}
+};
 
 export default memo(UserImage);
